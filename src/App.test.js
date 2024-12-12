9057a2f9f1105593213d83bfc586eaca6ef90abd
Atul Raj <55025161+AtulRaj06@users.noreply.github.com>
2024-12-12 09:34:17 +0530

    Update App.test.js for tdd test cases

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,23 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import {sum} from utils;
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('sum function', ()=>{
+    it('should return sum',()=>{
+        expect(sum('1,2,3,4')).toBe(10)
+    })
+
+    it('should return sum if string empty',()=>{
+        expect(sum('')).toBe(0)
+    })
+
+    it('should return error in negetive num', ()=>{
+        expect(sum("1,-9")).toBe('negative numbers not allowed <negative_number>')
+    })
+
+    it('should return error in negetive num', ()=>{
+        expect(sum("1\n2,3")).toBe('6')
+    })
+
+    it('should change delimeter', ()=>{
+        expect(sum("//;\n1;2")).toBe('3')
+    })
+})
